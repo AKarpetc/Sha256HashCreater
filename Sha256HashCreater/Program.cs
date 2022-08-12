f4e3162e90fee3865482ac909fd86f29f379d248
artyom <artyom.karpets@softeq.com>
2022-08-12 17:45:09 +0600

    Добавьте файлы проекта.

---
+++ Sha256HashCreater/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Sha256HashCreater
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+
+
+            var guid = Guid.NewGuid().ToString();
+
+            Console.WriteLine("Key: " + guid);
+
+            var hash = Sha256(guid);
+
+            Console.WriteLine("Hash: " + hash);
+
+       
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+
+        public static string Sha256(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return string.Empty;
+
+            using (var sha = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+
+
+}
